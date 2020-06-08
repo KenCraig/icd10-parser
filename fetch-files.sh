b6215ebe47e0041980787758aab0bb5da719e838
Ken Craig <kcraig@trustedcare.us>
2020-06-08 18:00:18 -0400

    fetch-files now works for 2017-2020

--- fetch-files.sh
+++ fetch-files.sh
@@ -6,20 +6,34 @@
 mkdir tmp
 pushd tmp
 
-CODEFILE="2020-ICD-10-CM-Codes.zip"
+if [ $1 -eq 2017 ]; then
+  CODEFILE="2017-ICD10-Code-Descriptions.zip"
+  TABLEFILE="2017-ICD10-Code-Tables-Index.zip"
+elif [ $1 -eq 2018 ]; then
+  CODEFILE="2018-ICD-10-Code-Descriptions.zip"
+  TABLEFILE="2018-ICD-10-Table-And-Index.zip"
+elif [ $1 -eq 2019 ]; then
+  CODEFILE="2019-ICD-10-CM-Code-Descriptions.zip"
+  TABLEFILE="2019-ICD-10-CM-Tables-and-Index.zip"
+elif [ $1 -eq 2020 ]; then
+  CODEFILE="2020-ICD-10-CM-Codes.zip"
+  TABLEFILE="2020-ICD-10-CM-Code-Tables.zip"
+else
+  print Usage: $0 [year]
+fi
+
 if [ ! -f $CODEFILE ]; then
   curl -O "https://www.cms.gov/Medicare/Coding/ICD10/Downloads/"${CODEFILE}
 fi
 
 if [ -f $CODEFILE ]; then
-  unzip -oj ${CODEFILE} "*/icd10cm_order_????.txt"
+  unzip -oj ${CODEFILE} "*/icd10cm_order_????.txt" || unzip -oj ${CODEFILE} "icd10cm_order_????.txt"
 fi
 
-TABLEFILE="2020-ICD-10-CM-Code-Tables.zip"
 if [ ! -f $TABLEFILE ]; then
   curl -O "https://www.cms.gov/Medicare/Coding/ICD10/Downloads/"${TABLEFILE}
 fi
 if [ -f $TABLEFILE ]; then
-  unzip -oj ${TABLEFILE} "*/icd10cm_tabular_????.xml"
+  unzip -oj ${TABLEFILE} "*/icd10cm_tabular_????.xml" || unzip -oj ${TABLEFILE} "icd10cm_tabular_????.xml"
 fi
 popd
